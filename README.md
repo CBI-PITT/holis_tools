8cbccaf790adef1e44999c7564adadf8c7ed58d5
Alan M Watson <alan.watson@pitt.edu>
2023-06-22 21:37:44 -0400

    Tools for teading spool files from compression_tools zip archivs

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# holis_tools
+# compression_tools
+
+## This repository offers tools to assist in file compression
+
+#### Installing:
+
+```bash
+# Clone the repo
+cd /dir/of/choice
+git clone https://github.com/CBI-PITT/holis_tools.git
+
+# Create a virtual environment
+# This assumes that you have miniconda or anaconda installed
+conda create -n holis_tools python=3.8 -y
+
+# Activate environment and install zarr_stores
+conda activate holis_tools
+pip install -e /dir/of/choice/holis_tools
+```
+
+
+
+##### <u>spool_zip_collection:</u>
+
+###### Description:
+
+This tool enables a .zip file containing a collection of spools files that represents 1 strip to be read and formatted as numpy arrays for downstream processing. Currently the package assumes that the zip file is formatted according to the compression_tools library found here: https://github.com/CBI-PITT/compression_tools/tree/main/compression_tools
+
+###### Usage example:
+
+```bash
+Stay tuned!
+```
+
+###### Note:
+
---
+++ holis_tools/spool_reader.py
@@ -0,0 +1,104 @@
+
+
+from compression_tools.alt_zip import alt_zip
+import configparser
+import numpy as np
+
+test_spool_zip = r'H:\globus\pitt\bil\hillman\spool_examples\COMPRESSED_CLEV5_Planes_secondColor_run12__z01_y12_Exc_488nm_660nm.zip'
+
+spool_set = alt_zip(test_spool_zip)
+
+# with open(r'H:\globus\pitt\bil\hillman\spool_examples\Planes_secondColor_run12__z01_y12_Exc_488nm_660nm\acquisitionmetadata.ini','r') as f:
+#     print(f.read())
+
+class spool_set_interpreter:
+
+    def __init__(self,compression_tools_zip_file):
+        self.compression_tools_zip_file = compression_tools_zip_file
+        self.location = compression_tools_zip_file
+        self.spool_set = alt_zip(self.location)
+
+        self._list_spool_files()
+        self._get_acquisitionparameters_str()
+        self._get_config()
+        self._extract_config_values()
+
+
+    @property
+    def entries(self):
+        return self.spool_set.entries
+
+    def _list_spool_files(self):
+        self.spool_files = sorted(
+            tuple(
+            [x for x in self.entries if '0spool.dat' in x]
+        )
+        )
+    def _get_acquisitionparameters_str(self):
+        ini = self.spool_set['acquisitionmetadata.ini']
+        ini = ini.decode('UTF-8-sig')  # Encoding for acquisitionmetadata.ini
+        self.acquisitionparameters_str = ini
+
+    def _get_config(self):
+        buf = io.StringIO(self.acquisitionparameters_str)
+        self.config = configparser.ConfigParser()
+        self.config.read_file(buf)
+
+    def _extract_config_values(self):
+        self.acquisition_metadata = {}
+        # ini info
+        self.acquisition_metadata['height'] = config.getint('data', 'AOIHeight')
+        self.acquisition_metadata['width'] = config.getint('data', 'AOIWidth')
+        self.acquisition_metadata['stride'] = config.getint('data', 'AOIStride')
+        dtype = config.get('data', 'PixelEncoding')
+
+        if dtype == 'Mono16':
+            dtype = np.dtype('uint16')
+        elif dtype == 'Mono8':
+            dtype = np.dtype('uint8')
+
+        self.acquisition_metadata['dtype'] = dtype
+        self.dtype = dtype
+
+        self.nbytes = config.getint('data', 'ImageSizeBytes')
+        self.acquisition_metadata['nbytes'] = self.nbytes
+
+        self.acquisition_metadata['images'] = config.getint('multiimage', 'ImagesPerFile')
+
+        numDepths = self.acquisition_metadata['height']
+        numLatPix = self.acquisition_metadata['stride'] // 2
+        imageBytes = self.acquisition_metadata['nbytes']
+        numFramesPerSpool = self.acquisition_metadata['images']
+        startIndex = self.acquisition_metadata['nbytes']
+        imageSize = self.acquisition_metadata['nbytes']
+
+        numRows = numDepths + 2
+
+        if numDepths % 2:  # if there is an odd number of rows ->  KPEDIT - odd rows means 1 less column for some reason
+            numRows = numDepths + 1
+
+        numColumns = numLatPix
+
+        self.spool_shape = (numFramesPerSpool, numRows, numColumns)
+
+    def _load_spool_file(self,spool_file_name):
+        array = np.frombuffer(self.spool_set[spool_file_name],dtype=a.dtype)
+        return np.reshape(array, self.spool_shape)
+
+    def __getitem__(self,key):
+        if isinstance(key,str):
+            assert key in self.spool_files, 'Must be a spool file in self.spool_files OR integer index in self.spool_files'
+            return self._load_spool_file(key)
+        elif isinstance(key,int):
+            return self._load_spool_file(self.spool_files[key])
+
+    def __iter__(self):
+        yield from (self[x] for x in range(len(self.spool_files)))
+
+    def __contains__(self, item):
+        return item in self.spool_files
+
+
+a = spool_set_interpreter(test_spool_zip)
+a._load_spool_file('8970000000spool.dat')
+
