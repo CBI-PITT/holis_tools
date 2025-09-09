e93c172f1666fceb666f8b02801e93a7d6d88e5e
Alan M Watson <alan.watson@pitt.edu>
2025-09-09 18:01:42 -0400

    test

--- holis_tools/hicam_utils.py
+++ holis_tools/hicam_utils.py
@@ -1396,29 +1396,31 @@ if __name__ == "__main__":
 
     import os, time
 
-    base = '/CBI_FastStore/hillman/test_run_small'
-
-    spool_files = (
-        'longRun3-y1-z0_HiCAM FLUO_1875-ST-088.fli',
-        'longRun4-y1-z1_HiCAM FLUO_1875-ST-088.fli',
-        'longRun3-y1-z0_HiCAM FLUO_1875-ST-272.fli',
-        'longRun4-y1-z1_HiCAM FLUO_1875-ST-272.fli'
-    )
-
-    spool_files = [os.path.join(base,x) for x in spool_files]
-
-    # zarr_locations = [os.path.join(r'/bil/users/awatson/holis',os.path.split(x)[-1] + '_ZARR_OUT') for x in spool_files]
-    zarr_locations = [x + '_ZARR_OUT' for x in spool_files]
-
-    # def run():
-    #     start = time.time()
-    #     for spool_file, zarr_location in zip(spool_files, zarr_locations):
-    #         send_hicam_to_zarr_par_read_once(spool_file,zarr_location,compressor_type='zstd', compressor_level=5, shuffle=1, chunk_depth=128, frames_at_once=128)
-    #     stop = time.time()
-    #     print(f'Total time = {stop}')
-
-
-    # run()
-    a = [open_hicam_array_as_aligned_color_dataset(zarr_locations[0])]
-    # a = [open_hicam_array_as_aligned_color_dataset(x) for x in zarr_locations]
+    file = "/bil/proj/rf1hillman/HOLiS_NPBB328_Cortex/Slab6/2025_08_22_HOLiS_NPBB328_Cortex_Slab06/NPBB328-Cortex-Slab06-run999-z13-y044-Exc-488nm-561nm-594nm-660nm_HiCAM FLUO_1875-ST-272.fli.zst"
+    #
+    # base = '/CBI_FastStore/hillman/test_run_small'
+    #
+    # spool_files = (
+    #     'longRun3-y1-z0_HiCAM FLUO_1875-ST-088.fli',
+    #     'longRun4-y1-z1_HiCAM FLUO_1875-ST-088.fli',
+    #     'longRun3-y1-z0_HiCAM FLUO_1875-ST-272.fli',
+    #     'longRun4-y1-z1_HiCAM FLUO_1875-ST-272.fli'
+    # )
+    #
+    # spool_files = [os.path.join(base,x) for x in spool_files]
+    #
+    # # zarr_locations = [os.path.join(r'/bil/users/awatson/holis',os.path.split(x)[-1] + '_ZARR_OUT') for x in spool_files]
+    # zarr_locations = [x + '_ZARR_OUT' for x in spool_files]
+    #
+    # # def run():
+    # #     start = time.time()
+    # #     for spool_file, zarr_location in zip(spool_files, zarr_locations):
+    # #         send_hicam_to_zarr_par_read_once(spool_file,zarr_location,compressor_type='zstd', compressor_level=5, shuffle=1, chunk_depth=128, frames_at_once=128)
+    # #     stop = time.time()
+    # #     print(f'Total time = {stop}')
+    #
+    #
+    # # run()
+    # a = [open_hicam_array_as_aligned_color_dataset(zarr_locations[0])]
+    # # a = [open_hicam_array_as_aligned_color_dataset(x) for x in zarr_locations]
 
